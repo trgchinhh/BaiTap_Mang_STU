d769c25178ec7bf258ae94b2e271927efeb073c6
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2025-05-14 11:41:56 +0700

    Update chuongtrinh.cpp

--- BaiTap_Mang_2_Chieu/Bai_tap_mang_bat_ky/chuongtrinh.cpp
+++ BaiTap_Mang_2_Chieu/Bai_tap_mang_bat_ky/chuongtrinh.cpp
@@ -32,4 +32,17 @@ int main(){
     tim_shh_min_ma_tran_so_nguyen(a, n, m);
     liet_ke_cac_dong_sum_max_ma_tran_so_nguyen(a, n, m);
     liet_ke_dong_co_nhieu_snt_ma_tran_so_nguyen(a, n, m);
+
+    // III. TÍNH TOÁN MA TRẬN 
+    tinh_tong_pt_ma_tran_so_nguyen(a, n, m);
+    tinh_tich_so_le_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_gt_duong_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_so_chia_het_cho_3_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_boi_cua_2_va_7_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_snt_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_shh_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_tbc_snt_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_tbc_scp_ma_tran_so_nguyen(a, n, m);
+    tinh_tong_tbn_shh_ma_tran_so_nguyen(a, n, m);
+
 }
